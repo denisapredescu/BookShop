9d94f37b85aee173dd92b5f4f015d945e18c19f3
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2024-03-29 13:51:49 +0200

    author frontend CRUD

--- src/main/java/com/awbd/bookshop/apis/AuthorController.java
+++ src/main/java/com/awbd/bookshop/apis/AuthorController.java
@@ -4,10 +4,13 @@ package com.awbd.bookshop.apis;
 import com.awbd.bookshop.dtos.RequestAuthor;
 import com.awbd.bookshop.mappers.AuthorMapper;
 import com.awbd.bookshop.models.Author;
+import com.awbd.bookshop.models.Category;
 import com.awbd.bookshop.services.author.IAuthorService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -22,30 +25,81 @@ public class AuthorController {
         this.mapper = mapper;
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<Author> addAuthor(
-            @Valid @RequestBody RequestAuthor newAuthor) {
-        return ResponseEntity.ok(authorService.addAuthor(mapper.requestAuthor(newAuthor)));
+//    @PostMapping("/add")
+//    public ResponseEntity<Author> addAuthor(
+//            @Valid @RequestBody RequestAuthor newAuthor) {
+//        return ResponseEntity.ok(authorService.addAuthor(mapper.requestAuthor(newAuthor)));
+//    }
+    @PostMapping("")
+    public ModelAndView save(
+            @Valid @ModelAttribute Author author){
+        authorService.addAuthor(author);
+        return new ModelAndView("redirect:/author");
     }
 
-    @PatchMapping("/update/{id}")
-    public ResponseEntity<Author> updateAuthor(
+    @PostMapping("/update")
+    public ModelAndView saveAuthorUpdate(
+            @Valid @ModelAttribute Author author){
+        authorService.updateAuthor(author,author.getId());
+        return new ModelAndView("redirect:/author");
+    }
+    @RequestMapping("/add")
+    public ModelAndView addAuthor(
+            @Valid Model model,
+            @Valid RequestAuthor newAuthor){
+        model.addAttribute("author",mapper.requestAuthor(newAuthor));
+        return new ModelAndView("authorAddForm");
+    }
+//    @PatchMapping("/update/{id}")
+//    public ResponseEntity<Author> updateAuthor(
+//            @PathVariable int id,
+//            @Valid @RequestBody RequestAuthor updateAuthor) {
+//        return ResponseEntity.ok(authorService.updateAuthor(mapper.requestAuthor(updateAuthor), id));
+//    }
+//@PatchMapping("/update/{id}")
+//public ModelAndView uAuthor(
+//        @PathVariable int id,
+//        @Valid @ModelAttribute RequestAuthor author) {
+//    System.out.println(id+"");
+//    authorService.updateAuthor(mapper.requestAuthor(author), id);
+//    return new ModelAndView("redirect:/author");
+//}
+
+    @RequestMapping("/update/{id}") //cand merg pe ruta asta doar se afiseaza categoryForm
+    public ModelAndView updateAuthor(
             @PathVariable int id,
-            @Valid @RequestBody RequestAuthor updateAuthor) {
-        return ResponseEntity.ok(authorService.updateAuthor(mapper.requestAuthor(updateAuthor), id));
+            @Valid Model model){
+
+        model.addAttribute("author",authorService.getAuthorById(id));
+
+
+        return new ModelAndView("authorForm");
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteAuthor(
-            @PathVariable int id) {
+//    @DeleteMapping("/delete/{id}")
+//    public ResponseEntity<Void> deleteAuthor(
+//            @PathVariable int id) {
+//        authorService.deleteAuthor(id);
+//        return ResponseEntity.noContent().build();
+//    }
+    @RequestMapping("/delete/{id}")
+    public ModelAndView deleteAuthor(
+            @PathVariable int id
+    ){
         authorService.deleteAuthor(id);
-        return ResponseEntity.noContent().build();
+        return new ModelAndView("redirect:/author");
     }
 
-    @GetMapping("/getAuthors")
-    public ResponseEntity<List<Author>> getAuthors(){
-        return ResponseEntity.ok(authorService.getAuthors());
-    }
+//    @GetMapping("/getAuthors")
+//    public ResponseEntity<List<Author>> getAuthors(){
+//        return ResponseEntity.ok(authorService.getAuthors());
+//    }
+@RequestMapping("")
+public ModelAndView getAuthors(Model model){
+    List<Author> authors = authorService.getAuthors();
+    model.addAttribute("authors",authors);
+    return new ModelAndView ("authorList");
+}
 
     @GetMapping("/getAuthor/{firstName}/{lastName}")
     public ResponseEntity<Author> getAuthor(
