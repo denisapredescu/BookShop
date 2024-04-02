d49ad86505868da551acea5133b8560d0349c062
denisapredescu <denissapredescu@yahoo.com>
2024-04-02 23:10:58 +0300

    Modified user model and repository

--- src/main/java/com/awbd/bookshop/repositories/UserRepository.java
+++ src/main/java/com/awbd/bookshop/repositories/UserRepository.java
@@ -5,10 +5,12 @@ import com.awbd.bookshop.dtos.UserResponse;
 import com.awbd.bookshop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     @Query("SELECT DISTINCT user.id, user.email, user.authority.authority FROM User user WHERE user.email = :email AND user.password = :password")
     Optional<UserResponse> getUser(String email, String password);
