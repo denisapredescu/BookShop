c3c4e27b50c1e1ba9021320b56d7b175b015c78f
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2024-04-12 10:53:04 +0300

    add ActiveProfile instead of Profile in Controller tests

--- src/test/java/com/awbd/bookshop/apis/BookControllerTest.java
+++ src/test/java/com/awbd/bookshop/apis/BookControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -31,7 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
-@Profile("mysql")
+@ActiveProfiles("mysql")
 public class BookControllerTest {
     @Autowired
     private MockMvc mockMvc;
