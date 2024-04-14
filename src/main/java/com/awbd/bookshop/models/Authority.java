313f9a5df55b1f2301a7175743adbf7a80317bc6
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2024-04-15 02:50:41 +0300

    Prettify code

--- src/main/java/com/awbd/bookshop/models/Authority.java
+++ src/main/java/com/awbd/bookshop/models/Authority.java
@@ -13,23 +13,8 @@ public class Authority {
     @Column(name = "authority", nullable = false, length = 50)
     private String authority;
 
-//    @ManyToOne
-//    @JoinColumn(name = "id", referencedColumnName = "id")
-//    private User user;
-
-//    @OneToMany(targetEntity = User.class)
-//    @PrimaryKeyJoinColumn(name = "user_id")
-//    private Set<User> users;
-
     public Authority() {
     }
-
-    public Authority(int id, String authority, Set<User> users) {
-        this.id = id;
-        this.authority = authority;
-       // this.users = users;
-    }
-
     public Authority(int id, String authority) {
         this.id = id;
         this.authority = authority;
@@ -50,52 +35,4 @@ public class Authority {
     public void setAuthority(String authority) {
         this.authority = authority;
     }
-
-//    public Set<User> getUsers() {
-//        return users;
-//    }
-
-//    public void setUsers(Set<User> users) {
-//        this.users = users;
-//    }
-
-    //    public Authority(int id, String authority, User user) {
-//        this.id = id;
-//        this.authority = authority;
-//        this.user = user;
-//    }
-//
-//    public Authority(String authority, User user) {
-//        this.authority = authority;
-//        this.user = user;
-//    }
-//
-//    public Authority(int id, String authority) {
-//        this.id = id;
-//        this.authority = authority;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getAuthority() {
-//        return authority;
-//    }
-//
-//    public void setAuthority(String authority) {
-//        this.authority = authority;
-//    }
-//
-//    public User getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(User user) {
-//        this.user = user;
-//    }
 }
