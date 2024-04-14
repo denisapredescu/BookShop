d84c16d367ddc62b20b5f7dc7446f57954b4031b
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2024-04-15 02:24:02 +0300

    Frontend design part 2

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -4,20 +4,20 @@ values (1,'ROLE_USER');
 insert into authorities(id, authority)
 values (2,'ROLE_ADMIN');
 
-insert into users(username,email,password,first_name,last_name,enabled,authority_id)
-values ('user12','user12@yahoo.com','pass','lara','sorana',true,1);
-
-insert into users(username,email,password,first_name,last_name,enabled,authority_id)
-values ('admin12','admin12@yahoo.com','pass','sina','mia',true,2);
-
-insert into categories(id,name) values (1,'romantic');
-insert into categories(id,name) values (2,'fictiune');
-
-insert into users(username,email,password,first_name,last_name,enabled,authority_id)
-values ('ana12','ana@yahoo.com','pass','ana','paraschiv',true,1);
-
-insert into users(username,email,password,first_name,last_name,enabled,authority_id)
-values ('anda12','anda@yahoo.com','pass','anda','paraschiva',true,1);
-
-insert into users(username,email,password,first_name,last_name,enabled,authority_id)
-values ('mara12','mara@yahoo.com','pass','mara','santila',true,1);
+-- insert into users(username,email,password,first_name,last_name,enabled,authority_id)
+-- values ('user12','user12@yahoo.com','pass','lara','sorana',true,1);
+--
+-- insert into users(username,email,password,first_name,last_name,enabled,authority_id)
+-- values ('admin12','admin12@yahoo.com','pass','sina','mia',true,2);
+--
+-- insert into categories(id,name) values (1,'romantic');
+-- insert into categories(id,name) values (2,'fictiune');
+--
+-- insert into users(username,email,password,first_name,last_name,enabled,authority_id)
+-- values ('ana12','ana@yahoo.com','pass','ana','paraschiv',true,1);
+--
+-- insert into users(username,email,password,first_name,last_name,enabled,authority_id)
+-- values ('anda12','anda@yahoo.com','pass','anda','paraschiva',true,1);
+--
+-- insert into users(username,email,password,first_name,last_name,enabled,authority_id)
+-- values ('mara12','mara@yahoo.com','pass','mara','santila',true,1);
