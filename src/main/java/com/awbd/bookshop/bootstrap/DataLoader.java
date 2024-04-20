a1efb67ab283c5f99f5e421e1751e7658b52b1b9
denisapredescu <denissapredescu@yahoo.com>
2024-04-20 14:07:05 +0300

    Modified dataloader info

--- src/main/java/com/awbd/bookshop/bootstrap/DataLoader.java
+++ src/main/java/com/awbd/bookshop/bootstrap/DataLoader.java
@@ -24,7 +24,7 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void loadUserData() {
-        if (userRepository.count() == 0){//5
+        if (userRepository.count() == 0) {
             Authority authority_admin = new Authority("ROLE_ADMIN");
             authorityRepository.save(authority_admin);
 
@@ -33,19 +33,19 @@ public class DataLoader implements CommandLineRunner {
 
             User admin = new User();
             admin.setUsername("admin");
-            admin.setEmail("adminul@yahoo.com");
-            admin.setPassword(passwordEncoder.encode("pass"));
-            admin.setFirstName("Sara");
-            admin.setLastName("Pan");
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword(passwordEncoder.encode("password"));
+            admin.setFirstName("admin_firstname");
+            admin.setLastName("admin_lastname");
             admin.setAuthority(authority_admin);
             userRepository.save(admin);
 
             User user = new User();
-            user.setUsername("miruna");
-            user.setEmail("miruna@yahoo.com");
-            user.setPassword(passwordEncoder.encode("pass"));
-            user.setFirstName("Miruna");
-            user.setLastName("Pos");
+            user.setUsername("user");
+            user.setEmail("user@gmail.com");
+            user.setPassword(passwordEncoder.encode("password"));
+            user.setFirstName("user_firstname");
+            user.setLastName("user_lastname");
             user.setAuthority(authority_user);
             userRepository.save(user);
         }
